4c4a296515007f73f1183d0ac2ecc167528b2bd6
Garu_laptop <lolpmmo@gmail.com>
2025-09-02 23:29:35 +0200

    cleaning

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -77,11 +77,11 @@ int	main(int ac, char **av)
 		if (fd == -1)
 			return (-1);
 	}
-	line = ft_get_line(fd);
+	line = get_next_line(fd);
 	while (line)
 	{
 		printf("%s", line);
-		line = ft_get_line(fd);
+		line = get_next_line(fd);
 	}
 	free(line);
 }
