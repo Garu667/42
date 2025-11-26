9d94ef0e092c113a9b97c6da1265b404898d5f81
Garu_laptop <lolpmmo@gmail.com>
2025-11-26 01:07:20 +0100

    tree

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramaroud <ramaroud@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 15:05:26 by ramaroud          #+#    #+#             */
+/*   Updated: 2025/11/25 11:19:54 by ramaroud         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	char			buffer[BUFFER_SIZE + 1];
+	int				fd;
+	struct s_list	*next;
+}	t_list;
+
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char s2[]);
+int		ft_check_line(char *line);
+void	ft_format(char **line, char buffer[]);
+int		ft_lstadd_back(t_list **lst, int fd);
+char	*get_next_line(int fd);
+void	ft_delnode(t_list **lst, int fd);
+char	*ft_clean(t_list **lst, char *to_free, int fd, int flag);
+
+#endif
---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramaroud <ramaroud@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 15:03:58 by ramaroud          #+#    #+#             */
+/*   Updated: 2025/11/24 19:27:56 by ramaroud         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char s2[])
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = -1;
+	j = ft_strlen(s1);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	if (ft_strlen(s1) > 0)
+	{
+		while (s1[++i])
+			str[i] = s1[i];
+	}
+	i = 0;
+	if (ft_strlen(s2) > 0)
+	{
+		while (s2[i])
+			str[j++] = s2[i++];
+	}
+	free(s1);
+	str[j] = 0;
+	return (str);
+}
+
+int	ft_check_line(char *line)
+{
+	int	i;
+
+	i = -1;
+	if (!line || line[0] == 0)
+		return (-1);
+	while (line[++i])
+		if (line[i] == '\n')
+			return (i);
+	return (-1);
+}
+
+void	ft_format(char **line, char buffer[])
+{
+	int	i;
+	int	j;
+
+	j = -1;
+	i = ft_check_line(*line) + 1;
+	if (i == 0)
+		return ;
+	while ((*line)[i + ++j] && j != BUFFER_SIZE)
+		buffer[j] = (*line)[i + j];
+	buffer[j] = 0;
+	(*line)[i] = 0;
+}
+
+int	ft_lstadd_back(t_list **lst, int fd)
+{
+	t_list	*node;
+	t_list	*last;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (-1);
+	node->buffer[0] = 0;
+	node->fd = fd;
+	node->next = NULL;
+	if (!*lst)
+	{
+		*lst = node;
+		return (0);
+	}
+	last = *lst;
+	while (last->next)
+		last = last->next;
+	last->next = node;
+	return (0);
+}
