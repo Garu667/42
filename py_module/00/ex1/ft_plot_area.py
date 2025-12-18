fcabeca45d68f2ca13c7c33b2652b7cd3e2232cc
Garu_laptop <lolpmmo@gmail.com>
2025-12-18 23:32:31 +0100

    pymodule, rm libft

--- py_module/00/ex1/ft_plot_area.py
+++ py_module/00/ex1/ft_plot_area.py
@@ -2,4 +2,4 @@ def ft_plot_area():
     length = int(input("Enter length: "))
     width = int(input("Enter width: "))
     ret = width * length
-    print("Plot area:", ret)
+    print(f"Plot area: {ret}")
