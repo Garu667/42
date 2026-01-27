ef1c8f90b9d38343f7371ac28a3cff2bd8cabf2c
Garu667 <lolpmmo@gmail.com>
2026-01-27 20:03:02 +0100

    Flag check

--- parsing/check_flags.c
+++ parsing/check_flags.c
@@ -25,15 +25,15 @@ static void	ft_check2(int *flag, int flag_bits)
 
 static void	ft_check(char *str, int *flag)
 {
-	if (ft_strncmp(str, "--bench", 7) == 0)
+	if (ft_strncmp(str, "--bench\0", 8) == 0)
 		ft_check2(flag, FLAG_BENCH);
-	else if (ft_strncmp(str, "--simple", 8) == 0)
+	else if (ft_strncmp(str, "--simple\0", 9) == 0)
 		ft_check2(flag, FLAG_SIMPLE);
-	else if (ft_strncmp(str, "--medium", 8) == 0)
+	else if (ft_strncmp(str, "--medium\0", 9) == 0)
 		ft_check2(flag, FLAG_MEDIUM);
-	else if (ft_strncmp(str, "--complex", 10) == 0)
+	else if (ft_strncmp(str, "--complex\0", 11) == 0)
 		ft_check2(flag, FLAG_COMPLEX);
-	else if (ft_strncmp(str, "--adaptive", 10) == 0)
+	else if (ft_strncmp(str, "--adaptive\0", 11) == 0)
 		ft_check2(flag, FLAG_ADAPTIVE);
 	else
 		exit(write(2, "Error\n", 6));
