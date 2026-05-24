8d35ebf70fec10b54843c423314bf5028e738598
Hamza <156207364+Hamzx-code@users.noreply.github.com>
2026-05-24 17:22:15 +0200

    src/__init__.py: export parser classes and custom exceptions

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .config_parser import Parsing, MissingKeyError
+from .config_parser import PositionError, FileNameError
+
+__all__ = ["Parsing", "MissingKeyError", "PositionError", "FileNameError"]
